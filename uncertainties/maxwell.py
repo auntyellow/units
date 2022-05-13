ee0d50ab4ec1405c901228cd1418baafd5888251
auntyellow <webmaster@xqbase.com>
2022-05-13 20:20:10 +0800

    ∇×H = f(Jf, D)

--- uncertainties/maxwell.py
+++ uncertainties/maxwell.py
@@ -1,13 +1,14 @@
 from sympy import Eq, solve, symbols
 
-nabla, pt, c, aL, aH, aM, e0, m0, lam = symbols('∇, ∂_t, c, α_L, α_H, α_M, ε_0, µ_0, λ')
+nabla, pt, aL, e0, m0, lam = symbols('∇, ∂_t, α_L, ε_0, µ_0, λ')
 D, E, P = symbols('D, E, P')
 eq1 = Eq(D, lam*(e0*E + P))
 H, B, M = symbols('H, B, M')
-eq2 = Eq(H, lam*(aH*B/m0 - M))
+eq2 = Eq(H, lam*(aL**2*B/m0 - M))
 J, Jf, Jm, Jp = symbols('J, J_f, J_m, J_p')
 eq3 = Eq(J, Jf + Jm + Jp)
-eq4 = Eq(nabla*B, (m0*J + pt*E/c**2)/aL)
-eq5 = Eq(Jm, aM*nabla*M)
-eq6 = Eq(Jp, pt*P)
-# TODO: H = f(Jf, D)?
+eq4 = Eq(Jp, pt*P)
+eq5 = Eq(Jm, nabla*M/aL)
+eq6 = Eq(nabla*B, (m0*J + m0*e0*pt*E)/aL)
+s = solve([eq1, eq2, eq3, eq4, eq5, eq6], [E, P, J, H, B, M])
+print(f'∇×H = {nabla*s[H]}')
